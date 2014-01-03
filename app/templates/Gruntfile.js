1e34f158e1326b459641ccc38f0728e00b916088
ron gaulden <rgaulden@gmail.com>
2014-01-03 15:02:33 -0500

    use grunt-contib-connect for server

--- app/templates/Gruntfile.js
+++ app/templates/Gruntfile.js
@@ -3,67 +3,67 @@ module.exports = function(grunt) {
   grunt.initConfig({
 
    copy: {
-      main: {
-        expand: true,
-        cwd: 'bower_components/html5-boilerplate/',
-        src: ['**'],
-        dest: 'app/'
-          },
-      secondary: {
-        expand: true,
-        cwd: 'bower_components/Sass-File-Structure/',
-        src: ['**'],
-        dest: 'app/'
-      }
+    main: {
+      expand: true,
+      cwd: 'bower_components/html5-boilerplate/',
+      src: ['**'],
+      dest: 'app/'
     },
-    clean: {
-        main: {
-            src : ["app/html5-boilerplate/"]
-        },
-        secondary: {
-            src : ["app/css/*.css"]
-        }
+    secondary: {
+      expand: true,
+      cwd: 'bower_components/Sass-File-Structure/',
+      src: ['**'],
+      dest: 'app/'
+    }
+  },
+  clean: {
+    main: {
+      src : ["app/html5-boilerplate/"]
     },
+    secondary: {
+      src : ["app/css/*.css"]
+    }
+  },
 
-    replace: {
-      css: {
-        src: ['app/index.html'],
-        overwrite: true,
-          replacements: [{
-            from: '<link rel="stylesheet" href="css/normalize.css">',
-            to: ''
-          }, {
-            from: '<link rel="stylesheet" href="css/main.css">',
-            to: '<link rel="stylesheet" href="css/compiled/main.css">'
-          }]
-      }
-    },
+  replace: {
+    css: {
+      src: ['app/index.html'],
+      overwrite: true,
+      replacements: [{
+        from: '<link rel="stylesheet" href="css/normalize.css">',
+        to: ''
+      }, {
+        from: '<link rel="stylesheet" href="css/main.css">',
+        to: '<link rel="stylesheet" href="css/compiled/main.css">'
+      }]
+    }
+  },
 
     //node sass
     sass: {
-        dev: {
-            files: {
-                'app/css/compiled/main.css': 'app/css/source/main.scss',
-                'app/css/compiled/main-old-ie.css': 'app/css/source/main-old-ie.scss'
-            }
+      dev: {
+        files: {
+          'app/css/compiled/main.css': 'app/css/source/main.scss',
+          'app/css/compiled/main-old-ie.css': 'app/css/source/main-old-ie.scss'
         }
+      }
     },
 
     //watch -- right now only sass
     watch: {
-        options: {
-            livereload: true
-        },
-        css: {
-            files: ['app/css/source/*.scss'],
-            tasks: ['nodeSass']
-        },
-        html: {
-          files: ['app/*.html']
-        },
-        img: {
-          files: ['app/img/**.*']
-        }
+      options: {
+        livereload: true
+      },
+      css: {
+        files: ['app/css/source/*.scss'],
+        tasks: ['nodeSass']
+      },
+      html: {
+        files: ['app/*.html']
+      },
+      img: {
+        files: ['app/img/**.*']
+      }
     },
 
   execute: {
@@ -73,9 +73,19 @@ module.exports = function(grunt) {
           },
           src: ['server-live.js']
       }
+  },
+
+  connect: {
+    uses_defaults: {
+      options: {
+        livereload: true
+      }
+    }
   }
 
-  });
+
+
+});
 
   // load the tasks
   grunt.loadNpmTasks('grunt-contrib-copy');
@@ -84,12 +94,13 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-execute');
   grunt.loadNpmTasks('grunt-text-replace');
+  grunt.loadNpmTasks('grunt-contrib-connect');
 
   // define the tasks
-    grunt.registerTask('default', ['copy', 'clean', 'replace','sass']);
-    grunt.registerTask('nodeSass',['sass']);
-    grunt.registerTask('serve',['execute']);
-    grunt.registerTask('nodeWatch',['watch']);
-    grunt.registerTask('setup', ['copy', 'clean', 'replace', 'sass']);
+  grunt.registerTask('default', ['copy', 'clean', 'replace','sass']);
+  grunt.registerTask('nodeSass',['sass']);
+  grunt.registerTask('serve',['execute']);
+  grunt.registerTask('nodeWatch',['watch']);
+  grunt.registerTask('setup', ['copy', 'clean', 'replace', 'sass', 'connect', 'watch'  ]);
 
 };
